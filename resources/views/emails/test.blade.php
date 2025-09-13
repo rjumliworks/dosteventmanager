0372db0365345d7fe42697b86cbfe88ad14b373a
kradworkz <80160702+kradworkz@users.noreply.github.com>
2025-09-13 08:36:35 +0800

    Create test.blade.php

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,11 @@
+Hi [First Name],
+
+Thank you for registering with [Your App Name]! 🎉
+Your account has been successfully created.
+
+🚀 Next Steps
+While we finalize the release of our iOS and Android mobile apps, please stay tuned—they’ll be available very soon.
+Once the apps are live, here’s how you can log in:
+
+1️⃣ Open the [Your App Name] mobile app (iOS or Android) once available.
+2️⃣ Enter your registered email address: [user@email.com
