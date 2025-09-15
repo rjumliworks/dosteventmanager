d52d17f2130a6212426eca6e6e3881eb5d27f91a
kradworkz <80160702+kradworkz@users.noreply.github.com>
2025-09-15 14:28:43 +0800

    Update AvatarController.php

--- app/Http/Controllers/Api/AvatarController.php
+++ app/Http/Controllers/Api/AvatarController.php
@@ -8,23 +8,61 @@ use Illuminate\Http\Request;
 class AvatarController extends Controller
 {
     public function store(Request $request){
-        $request->validate([
-            'image' => 'required|image64:jpeg,jpg,png' // Assuming maximum file size is 2MB
-        ]);
 
-        $data = Participant::with('detail')->where('id',$request->id)->first();
-        if ($data->detail->avatar) {
-            Storage::disk('public')->delete($data->detail->avatar);
+        try {
+            $request->validate([
+                'image' => 'required|image64:jpeg,jpg,png' // Assuming maximum file size is 2MB
+            ]);
+
+            
+            if($validateUser->fails()){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validateUser->errors()
+                ], 401);
+            }
+
+           
+
+           
+            if($request->image){
+                $dd = $request->image;
+                $img = explode(',', $dd);
+                $ini =substr($img[0], 11);
+                $type = explode(';', $ini);
+                if($type[0] == 'png'){
+                    $image = str_replace('data:image/png;base64,', '', $dd);
+                }else{
+                    $image = str_replace('data:image/jpeg;base64,', '', $dd);
+                }
+                $image = str_replace(' ', '+', $image);
+                $imageName =  date('Y').'-'.date('mhis').'.'.$type[0];
+                
+                if(\File::put(public_path('images/avatars'). '/' . $imageName, base64_decode($image))){
+                    $data = Participant::with('detail')->where('id',$request->id)->first();
+                    if ($data->detail->avatar) {
+                        Storage::disk('public')->delete($data->detail->avatar);
+                    }
+
+                    $data->detail->avatar = $imageName;
+                    $data->detail->save();
+
+                }
+            }
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Profile updated successfully',
+                'data' => true
+            ], 200);
+
+        }catch(\Throwable $th){
+
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
         }
-
-        $imagePath = $request->file('image')->store('mobile-pictures', 'public');
-        $data->detail->avatar = $imagePath;
-        $data->detail->save();
-
-         return response()->json([
-            'status' => true,
-            'message' => 'Profile updated successfully',
-            'data' => true
-        ], 200);
     }
 }
