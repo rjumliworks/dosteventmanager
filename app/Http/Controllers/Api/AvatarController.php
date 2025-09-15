2e9d46e3ecfc49eb1a3ffe5f7fe8d8fafbe35973
kradworkz <80160702+kradworkz@users.noreply.github.com>
2025-09-15 14:20:33 +0800

    aw

--- app/Http/Controllers/Api/AvatarController.php
+++ app/Http/Controllers/Api/AvatarController.php
@@ -9,7 +9,8 @@ class AvatarController extends Controller
 {
     public function store(Request $request){
         $request->validate([
-            'image' => 'required|image64:jpeg,jpg,png|max:2048' // Assuming maximum file size is 2MB
+            'image' => 'required|image|mimes:jpeg,jpg,png|max:2048'
+            // 'image' => 'required|image64:jpeg,jpg,png|max:2048' // Assuming maximum file size is 2MB
         ]);
 
         $data = Participant::with('detail')->where('id',$request->id)->first();
