7c60163f82f6e2d245353f7cb64d569d1c80ce65
kradworkz <80160702+kradworkz@users.noreply.github.com>
2025-09-15 14:18:23 +0800

    aw

--- app/Http/Controllers/Api/AvatarController.php
+++ app/Http/Controllers/Api/AvatarController.php
@@ -9,7 +9,7 @@ class AvatarController extends Controller
 {
     public function store(Request $request){
         $request->validate([
-            'image' => 'required|image|max:2048' // Assuming maximum file size is 2MB
+            'image' => 'required|image64:jpeg,jpg,png|max:2048' // Assuming maximum file size is 2MB
         ]);
 
         $data = Participant::with('detail')->where('id',$request->id)->first();
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,5 +28,15 @@ class AppServiceProvider extends ServiceProvider
             LoginSuccessful::class,
             LoginFailed::class
         );
+
+        \Validator::extend('image64', function ($attribute, $value, $parameters, $validator) {
+            if($value != null){
+                $type = explode('/', explode(':', substr($value, 0, strpos($value, ';')))[1])[1];
+                if (in_array($type, $parameters)) {
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
