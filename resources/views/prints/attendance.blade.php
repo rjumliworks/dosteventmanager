f7d6794734baad94e4ba3e0e7629519c8450914d
imkrad <164296287+imkrad@users.noreply.github.com>
2025-09-18 18:59:51 +0800

    Update attendance.blade.php

--- resources/views/prints/attendance.blade.php
+++ resources/views/prints/attendance.blade.php
@@ -162,13 +162,13 @@
                         <td>{{ $attendee->is_ip ? '✔' : '' }}</td>
                         <td>
                             @if(!empty($attendee->participant->detail->signature_base64))
-                                <img src="{{ $attendee->participant->detail->signature_base64 }}" style="width:70px; height:auto;" alt="Signature">
+                                <img src="{{ $attendee->participant->detail->signature_base64 }}" style="width:80px; height:auto;" alt="Signature">
                             @else
                                 <span>No signature</span>
                             @endif
                         </td>
                         <td>
-                            <img src="{{ $attendee->image_base64 }}" style="width:70px; height:auto;" alt="Photo">
+                            <img src="{{ $attendee->image_base64 }}" style="width:80px; height:auto;" alt="Photo">
                         </td>
                     </tr>
                 @endforeach
