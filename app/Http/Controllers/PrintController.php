cb49d5f911c05421d39dbead95ff050c475edf6c
kradworkz <80160702+kradworkz@users.noreply.github.com>
2025-09-18 09:32:56 +0800

    Update PrintController.php

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -204,6 +204,7 @@ class PrintController extends Controller
     }
 
     public function lists(){
+        ini_set('memory_limit', '1024M');
         ini_set('max_execution_time', 0);
         set_time_limit(0);
         $data = Participant::with('detail.sex')->get();
