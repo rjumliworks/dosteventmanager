0bf0528fb25d108b222b42ca34c84c26131ec6a1
kradworkz <80160702+kradworkz@users.noreply.github.com>
2025-09-24 12:14:25 +0800

    Update CsfController.php

--- app/Http/Controllers/Api/CsfController.php
+++ app/Http/Controllers/Api/CsfController.php
@@ -55,7 +55,7 @@ class CsfController extends Controller
         $entry->refresh();
         broadcast(new SessionEvent(new FeedbackResource($entry),'rating'));
         $this->certificate($request->session_id,$request->participant_id);
-        $this->participation($request->session_id,$request->participant_id);
+        // $this->participation($request->session_id,$request->participant_id);
         return response()->json([
             'status' => true,
             'message' => 'CSF submitted successfully',
